b2cfae9a27847b8a08b13c190bc632b7d5268496
NetLive <sas.1800@mail.ru>
2025-05-31 09:58:49 +0300

    Fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -14,10 +14,9 @@ module.exports = {
       host: "158.160.100.179",
       ref: "origin/master",
       repo: "git@github.com:NetLive5/nodejs-pm2-deploy.git",
-
-      path: "/home/praktikum",
+      path: "/home/praktikum/nodejs-pm2-deploy",
       "post-deploy":
-        "cd nodejs-pm2-deploy/backend && npm ci && npm run build && cd .. && pm2 reload ecosystem.config.js --env production",
+        "cd backend && npm ci && npm run build && pm2 reload ecosystem.config.js --env production",
     },
   },
 };
