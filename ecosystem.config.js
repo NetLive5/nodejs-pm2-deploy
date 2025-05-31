c33bd04e8dd19fbb500304546c61bdd471014928
NetLive <sas.1800@mail.ru>
2025-05-31 12:07:01 +0300

    Fix: update pre-deploy to use IP

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,8 @@ module.exports = {
       ref: "origin/master",
       repo: "git@github.com:NetLive5/nodejs-pm2-deploy.git",
       path: "/home/praktikum/nodejs-pm2-deploy",
-      "pre-deploy": `scp backend/.env prod-server:/home/praktikum/nodejs-pm2-deploy/current/backend/.env`,
+
+      "pre-deploy": `scp backend/.env praktikum@158.160.100.179:/home/praktikum/nodejs-pm2-deploy/current/backend/.env`,
 
       "post-deploy":
         "cd current/backend && npm ci && npm run build && pm2 reload ecosystem.config.js --env production",
