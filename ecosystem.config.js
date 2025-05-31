ddea06d2dd1771c00ab3ff13bfece8576684b61c
NetLive <sas.1800@mail.ru>
2025-05-31 11:59:36 +0300

    Fix pm2 deploy path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,11 +12,12 @@ module.exports = {
   deploy: {
     production: {
       user: "praktikum",
-      host: "158.160.100.179",
+      host: "prod-server",
       ref: "origin/master",
       repo: "git@github.com:NetLive5/nodejs-pm2-deploy.git",
       path: "/home/praktikum/nodejs-pm2-deploy",
-      "pre-deploy": `scp -i /Users/netlive/.ssh/id_rsa backend/.env praktikum@158.160.100.179:/home/praktikum/nodejs-pm2-deploy/current/backend/.env`,
+      "pre-deploy": `scp -i ~/.ssh/id_ed25519 backend/.env praktikum@158.160.100.179:/home/praktikum/nodejs-pm2-deploy/current/backend/.env`,
+
       "post-deploy":
         "cd current/backend && npm ci && npm run build && pm2 reload ecosystem.config.js --env production",
     },
