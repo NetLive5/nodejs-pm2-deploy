664ba5eb0941610401e7f39459ca6aac413bc113
NetLive <sas.1800@mail.ru>
2025-05-31 12:35:09 +0300

    Fix: use escaped double quotes for scp in sh -c

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,6 @@ module.exports = {
       },
     },
   ],
-
   deploy: {
     production: {
       user: "praktikum",
@@ -16,9 +15,6 @@ module.exports = {
       ref: "origin/master",
       repo: "git@github.com:NetLive5/nodejs-pm2-deploy.git",
       path: "/home/praktikum/nodejs-pm2-deploy",
-
-      "pre-deploy":
-        "rsync -avz -e 'ssh -i /Users/netlive/.ssh/id_rsa' backend/.env praktikum@158.160.100.179:/home/praktikum/nodejs-pm2-deploy/current/backend/.env",
 
       "post-deploy":
         "cd current/backend && npm ci && npm run build && pm2 reload ecosystem.config.js --env production",
