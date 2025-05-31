6a74e61417ae11a047ae7cd768a08ed7c25d27e2
NetLive <sas.1800@mail.ru>
2025-05-31 10:57:35 +0300

    Fix pm2 deploy path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,3 @@
-require("dotenv").config();
-
-const {
-  DEPLOY_USER = "praktikum",
-  DEPLOY_HOST = "158.160.100.179",
-  DEPLOY_PATH = "/home/praktikum/nodejs-pm2-deploy",
-} = process.env;
-
 module.exports = {
   apps: [
     {
@@ -19,13 +11,11 @@ module.exports = {
 
   deploy: {
     production: {
-      user: DEPLOY_USER,
-      host: DEPLOY_HOST,
+      user: "praktikum",
+      host: "158.160.100.179",
       ref: "origin/master",
       repo: "git@github.com:NetLive5/nodejs-pm2-deploy.git",
-      path: DEPLOY_PATH,
-      // скопировать .env в папку backend на сервере
-      "pre-deploy": `scp backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend/.env`,
+      path: "/home/praktikum/nodejs-pm2-deploy",
       "post-deploy":
         "cd backend && npm ci && npm run build && pm2 reload ecosystem.config.js --env production",
     },
