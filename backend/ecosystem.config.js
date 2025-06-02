1af6ccfeb97c6b6bacf9f0161673d9a0e0ff72e2
NetLive <sas.1800@mail.ru>
2025-06-02 08:39:54 +0300

    фикс перед деплоем

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "mesto",
+      script: "dist/app.js",
+      cwd: "./backend",
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: "praktikum",
+      host: "158.160.100.179",
+      ref: "origin/master",
+      repo: "git@github.com:NetLive5/nodejs-pm2-deploy.git",
+      path: "/home/praktikum/nodejs-pm2-deploy",
+
+      "post-deploy":
+        "cd backend && npm ci && npm run build && pm2 reload ../ecosystem.config.js --env production",
+    },
+  },
+};
